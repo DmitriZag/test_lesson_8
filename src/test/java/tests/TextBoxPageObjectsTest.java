dd752ee5961e22f52831baf7594c45a63e7b59c1
Dmitrii Zagorulko <dmitryzag88@gmail.com>
2024-01-14 21:50:03 +0300

    Added new tests

---
+++ src/test/java/tests/TextBoxPageObjectsTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.TextBoxPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest extends TestBase {
+
+    TextBoxPage textBoxPage = new TextBoxPage();
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        textBoxPage.openPage()
+                .setName("Alex")
+                .setEmail("alex@egorov.com")
+                .setAddress("Some street 1")
+                .setPermanentAddress("Another street 1")
+                .submit()
+
+                .checkOutput("name", "Alex")
+                .checkOutput("email", "alex@egorov.com")
+                .checkOutput("currentAddress", "Some street 1")
+                .checkOutput("permanentAddress", "Another street 1");
+    }
+}
